593d61c6556ead8072b71622f952bbdf5a0e31a2
Auston Stewart <auston@shedbuilt.net>
2018-01-12 00:24:55 -0500

    Adding the toolchain build mode.

--- build.sh
+++ build.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-./configure --prefix=/usr \
-            --localstatedir=/var/lib/locate
-make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} install
-mkdir -v ${SHED_FAKEROOT}/bin
-mv -v ${SHED_FAKEROOT}/usr/bin/find ${SHED_FAKEROOT}/bin
-sed -i 's|find:=${BINDIR}|find:=/bin|' ${SHED_FAKEROOT}/usr/bin/updatedb
+case "$SHED_BUILDMODE" in
+    toolchain)
+        ./configure --prefix=/tools || return 1
+        ;;
+    *)
+        ./configure --prefix=/usr \
+                    --localstatedir=/var/lib/locate || return 1
+        ;;
+esac
+make -j $SHED_NUMJOBS || return 1
+make DESTDIR="$SHED_FAKEROOT" install || return 1
+
+if [ "$SHED_BUILDMODE" != 'toolchain' ]; then
+    mkdir -v "${SHED_FAKEROOT}/bin"
+    mv -v "${SHED_FAKEROOT}/usr/bin/find" "${SHED_FAKEROOT}/bin"
+    sed -i 's|find:=${BINDIR}|find:=/bin|' "${SHED_FAKEROOT}/usr/bin/updatedb"
+fi
