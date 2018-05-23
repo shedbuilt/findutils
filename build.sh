f2d130571436a599477efed3ffb21ce40d6a3aac
Auston Stewart <auston@shedbuilt.net>
2018-05-22 17:01:46 -1000

    Updating for blank package options

--- build.sh
+++ build.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-case "$SHED_BUILD_MODE" in
-    toolchain)
-        ./configure --prefix=/tools || exit 1
-        ;;
-    *)
-        ./configure --prefix=/usr \
-                    --localstatedir=/var/lib/locate || exit 1
-        ;;
-esac
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+# Configure
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[toolchain]}" ]; then
+    ./configure --prefix=/tools || exit 1
+else
+    ./configure --prefix=/usr \
+                --localstatedir=/var/lib/locate || exit 1
+fi
+
+# Build and Install
 make -j $SHED_NUM_JOBS &&
 make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
-if [ "$SHED_BUILD_MODE" != 'toolchain' ]; then
+
+# Rearrange
+if [ -z "${SHED_PKG_LOCAL_OPTIONS[toolchain]}" ]; then
     mkdir -v "${SHED_FAKE_ROOT}/bin" &&
     mv -v "${SHED_FAKE_ROOT}/usr/bin/find" "${SHED_FAKE_ROOT}/bin" &&
     sed -i 's|find:=${BINDIR}|find:=/bin|' "${SHED_FAKE_ROOT}/usr/bin/updatedb" || exit 1
